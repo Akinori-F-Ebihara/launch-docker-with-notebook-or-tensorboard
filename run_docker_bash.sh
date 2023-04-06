5bc7322e6ba5567d358854b6ab00aeb31acf1307
Akinori-F-Ebihara <aebihara@icloud.com>
2023-04-06 11:48:37 +0900

    Update run_docker_bash.sh

--- run_docker_bash.sh
+++ run_docker_bash.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
 # This script runs a Docker container with the specified version and port.
 # When -j or -t option is provided, Jupyter Notebook or TensorBoard is directly
